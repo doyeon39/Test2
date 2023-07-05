f9fa245a7fda7c62f2310cdfeff67952c198daf9
gumaggo <lauv.0309@gmail.com>
2023-07-04 20:28:29 -0700

    ..

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     lintOnSave : false,
+    publicPath : '<Test2>',
+    outputDir: './docs',
     pluginOptions: {
         proxy: {
             '/': {
